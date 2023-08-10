a4598ec4353b34f86a7251a682bda44778cade34
berr <blinenkovm@yandex.ru>
2023-08-10 22:39:57 +0300

    added left & right arrows, some buttons and paint function, brush size changer

--- yokkaPaint/yokkaPaint/Window.Designer.cs
+++ yokkaPaint/yokkaPaint/Window.Designer.cs
@@ -32,14 +32,20 @@
             paintBox = new PictureBox();
             arrowUpButton = new Button();
             arrowDownButton = new Button();
+            penButton = new Button();
+            eraserButton = new Button();
+            brushSizeController = new NumericUpDown();
+            arrowRightButton = new Button();
+            arrowLeftButton = new Button();
             ((System.ComponentModel.ISupportInitialize)paintBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)brushSizeController).BeginInit();
             SuspendLayout();
             // 
             // openFileButton
             // 
-            openFileButton.Location = new Point(12, 12);
+            openFileButton.Location = new Point(3, 2);
             openFileButton.Name = "openFileButton";
-            openFileButton.Size = new Size(94, 29);
+            openFileButton.Size = new Size(116, 29);
             openFileButton.TabIndex = 0;
             openFileButton.Text = "Open file";
             openFileButton.UseVisualStyleBackColor = true;
@@ -74,11 +80,62 @@
             arrowDownButton.UseVisualStyleBackColor = true;
             arrowDownButton.Click += arrowDown;
             // 
+            // penButton
+            // 
+            penButton.Location = new Point(125, 2);
+            penButton.Name = "penButton";
+            penButton.Size = new Size(94, 29);
+            penButton.TabIndex = 4;
+            penButton.Text = "Pen";
+            penButton.UseVisualStyleBackColor = true;
+            // 
+            // eraserButton
+            // 
+            eraserButton.Location = new Point(225, 2);
+            eraserButton.Name = "eraserButton";
+            eraserButton.Size = new Size(94, 29);
+            eraserButton.TabIndex = 5;
+            eraserButton.Text = "Eraser";
+            eraserButton.UseVisualStyleBackColor = true;
+            // 
+            // brushSizeController
+            // 
+            brushSizeController.Location = new Point(325, 4);
+            brushSizeController.Name = "brushSizeController";
+            brushSizeController.Size = new Size(43, 27);
+            brushSizeController.TabIndex = 6;
+            brushSizeController.Value = new decimal(new int[] { 4, 0, 0, 0 });
+            // 
+            // arrowRightButton
+            // 
+            arrowRightButton.Location = new Point(84, 387);
+            arrowRightButton.Name = "arrowRightButton";
+            arrowRightButton.Size = new Size(35, 29);
+            arrowRightButton.TabIndex = 7;
+            arrowRightButton.Text = "→";
+            arrowRightButton.UseVisualStyleBackColor = true;
+            arrowRightButton.Click += arrowRight;
+            // 
+            // arrowLeftButton
+            // 
+            arrowLeftButton.Location = new Point(3, 387);
+            arrowLeftButton.Name = "arrowLeftButton";
+            arrowLeftButton.Size = new Size(35, 29);
+            arrowLeftButton.TabIndex = 8;
+            arrowLeftButton.Text = "←";
+            arrowLeftButton.UseVisualStyleBackColor = true;
+            arrowLeftButton.Click += arrowLeft;
+            // 
             // yWindow
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(arrowLeftButton);
+            Controls.Add(arrowRightButton);
+            Controls.Add(brushSizeController);
+            Controls.Add(eraserButton);
+            Controls.Add(penButton);
             Controls.Add(arrowDownButton);
             Controls.Add(arrowUpButton);
             Controls.Add(paintBox);
@@ -87,6 +144,7 @@
             Text = "yokkaPaint C#";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)paintBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)brushSizeController).EndInit();
             ResumeLayout(false);
         }
 
@@ -94,8 +152,12 @@
 
         private Button openFileButton;
         public PictureBox paintBox;
-        Graphics canvas;
         private Button arrowUpButton;
         private Button arrowDownButton;
+        private Button penButton;
+        private Button eraserButton;
+        private NumericUpDown brushSizeController;
+        private Button arrowRightButton;
+        private Button arrowLeftButton;
     }
 }
--- yokkaPaint/yokkaPaint/Window.cs
+++ yokkaPaint/yokkaPaint/Window.cs
@@ -9,6 +9,15 @@ namespace yokkaPaint
         int paintBoxX = 125;
         int paintBoxY = 37;
         TextBox control = new TextBox();
+        Graphics canvas;
+        Pen pen;
+        bool drawMode = false;
+        System.Windows.Forms.Timer drawTicker;
+        int MouseX = 10;
+        int MouseY = 10;
+        int BrushSize = 4;
+
+        string Mode = "Pen";
 
         public yWindow()
         {
@@ -16,7 +25,6 @@ namespace yokkaPaint
             /*            control.Location = new Point(-100, -100);
                         this.Controls.Add(control);
                         control.KeyPress += new KeyPressEventHandler(keypress);*/
-            paintBox.CreateGraphics();
             InitializeComponent();
         }
 
@@ -25,10 +33,16 @@ namespace yokkaPaint
             switch (dir)
             {
                 case "w":
-                    paintBoxY -= 5;
+                    paintBoxY -= 50;
                     break;
                 case "s":
-                    paintBoxY += 5;
+                    paintBoxY += 50;
+                    break;
+                case "d":
+                    paintBoxX += 50;
+                    break;
+                case "a":
+                    paintBoxX -= 50;
                     break;
             }
 
@@ -39,16 +53,56 @@ namespace yokkaPaint
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Console.WriteLine("Test");
+            brushSizeController.ValueChanged += new EventHandler(changeBrushSize);
+
+        }
+
+        private void changeBrushSize(object sender, EventArgs e)
+        {
+            BrushSize = Convert.ToInt16(brushSizeController.Value);
+        }
+
+        private void DrawingModeOn(object sender, MouseEventArgs e) { drawMode = true; }
+        private void DrawingModeOff(object sender, MouseEventArgs e) { drawMode = false; }
+
+        private void UpdateCursorCoords(object sender, MouseEventArgs e)
+        {
+            MouseX = e.Location.X;
+            MouseY = e.Location.Y;
         }
 
         private void openFileButton_click(object sender, EventArgs e)
         {
             Bitmap openedImage = new Bitmap(@"C:\Users\bline\paint.jpg");
-            Graphics graphics = Graphics.FromImage(openedImage);
+            canvas = Graphics.FromImage(openedImage);
             paintBox.Image = openedImage;
+
             paintBox.Size = new Size(openedImage.Width, openedImage.Height);
 
+            paintBox.MouseDown += new MouseEventHandler(DrawingModeOn);
+            paintBox.MouseUp += new MouseEventHandler(DrawingModeOff);
+            paintBox.MouseMove += new MouseEventHandler(UpdateCursorCoords);
+
+            drawTicker = new System.Windows.Forms.Timer();
+            drawTicker.Interval = 20;
+            drawTicker.Tick += new EventHandler(PaintTick);
+            drawTicker.Start();
+        }
+
+
+        private void PaintTick(object sender, EventArgs e)
+        {
+            if (drawMode)
+            {
+                pen = new Pen(Brushes.Aqua);
+                pen.Width = BrushSize
+                    ;
+                pen.LineJoin = System.Drawing.Drawing2D.LineJoin.Bevel;
+                canvas.DrawRectangle(pen, MouseX, MouseY, BrushSize, BrushSize);
+
+                pen.Dispose();
+                paintBox.Invalidate();
+            }
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -58,12 +112,22 @@ namespace yokkaPaint
 
         private void arrowUp(object sender, EventArgs e)
         {
-            ChangePaintBoxPos("w");
+            ChangePaintBoxPos("s");
         }
 
         private void arrowDown(object sender, EventArgs e)
         {
-            ChangePaintBoxPos("s");
+            ChangePaintBoxPos("w");
+        }
+
+        private void arrowRight(object sender, EventArgs e)
+        {
+            ChangePaintBoxPos("d");
+        }
+
+        private void arrowLeft(object sender, EventArgs e)
+        {
+            ChangePaintBoxPos("a");
         }
     }
 }
